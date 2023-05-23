8d382f6f00b9732c9d8be576c9dd9f144b7b3431
Jordan Smith <46058723+jsmithnh09@users.noreply.github.com>
2023-05-23 14:29:45 -0400

    Removing `strcmpi` since func is implicit.

--- src/hashfile.c
+++ src/hashfile.c
@@ -20,16 +20,16 @@
 int main(int argc, char *argv[]) {
     char *output;
     if (argc >= 3) {
-        if (!strcmpi(argv[2], "md5")) {
+        if (!strcmp(argv[2], "md5")) {
             printf("MD5   : ");
             output = md5_file(argv[1]);
-        } else if (!strcmpi(argv[2], "sha256")) {
+        } else if (!strcmp(argv[2], "sha256")) {
             printf("SHA256: ");
             output = sha256_file(argv[1]);
-        } else if (!strcmpi(argv[2], "sha1")) {
+        } else if (!strcmp(argv[2], "sha1")) {
             printf("SHA1  : ");
             output = sha1_file(argv[1]);
-        } else if (!strcmpi(argv[2], "crc32")) {
+        } else if (!strcmp(argv[2], "crc32")) {
             printf("CRC32 : ");
             output =  crc32_file(argv[1]);
         } else {
