c911cb3101f6d4598a35e3f2e54cdcf860346139
Jordan Smith <46058723+jsmithnh09@users.noreply.github.com>
2023-05-23 10:16:24 -0400

    Update test_filehash.m

--- test/test_filehash.m
+++ test/test_filehash.m
@@ -6,8 +6,8 @@ classdef test_filehash < matlab.unittest.TestCase
     properties(Constant)
         ALGORITHMS = {'SHA1', 'SHA256', 'MD5', 'CRC32'};
         PS_PRINTF = 'Get-FileHash %s -Algorithm %s';
-        TEST_FILE = fullfile(fileparts(mfilename('fullpath')), 'test', 'test.txt');
-        RESULTS_FILE = fullfile(fileparts(mfilename('fullpath')), 'test', 'results.json');
+        TEST_FILE = fullfile(fileparts(mfilename('fullpath')), 'test.txt');
+        RESULTS_FILE = fullfile(fileparts(mfilename('fullpath')), 'results.json');
     end
 
     properties
@@ -18,7 +18,7 @@ classdef test_filehash < matlab.unittest.TestCase
     methods(TestClassSetup)
         function read_testfile(testCase)
             % READ_TESTFILE reads the results from PowerShell and Python3 into MATLAB.
-            testCase.file_hashes = jsondecode(testCase.RESULTS_FILE);
+            testCase.file_hashes = jsondecode(fileread(testCase.RESULTS_FILE));
         end
     end
 
