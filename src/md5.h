0bd331bf04b2b195acd57dbd75a3d2cf0c867ef1
Jordan Smith <46058723+jsmithnh09@users.noreply.github.com>
2023-05-23 14:30:34 -0400

    Update md5.h

--- src/md5.h
+++ src/md5.h
@@ -37,5 +37,6 @@ void md5_init(MD5_CTX *ctx);
 void md5_update(MD5_CTX *ctx, const BYTE data[], size_t len);
 void md5_final(MD5_CTX *ctx, BYTE hash[]);
 char* md5_file(const char* filename);
+void md5_closefile(void);
 
 #endif   // MD5_H
