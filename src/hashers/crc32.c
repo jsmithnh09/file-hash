c1f6bc08e57f18764bc5a9c752f3e79323fbb62a
jsmithnh09 <jsmithnh09@gmail.com>
2023-09-03 16:49:10 -0400

    Adding `_bytes` signature for CRC32.

--- src/hashers/crc32.c
+++ src/hashers/crc32.c
@@ -127,3 +127,20 @@ char* crc32_file(const char* filename)
     fclose(fid);
     return fileprint;
 }
+
+char* crc32_bytes(const void* buffer, size_t nbElements)
+{
+	BYTE hashbuff[CRC32_BLOCK_SIZE];
+	BYTE *byteBuff = (BYTE)(buffer);
+	size_t buffIdx = 0;
+	CRC32_CTX ctx;
+	char *fileprint = (char*)calloc((CRC32_STRLEN)+1, sizeof(char));
+	crc32_init(&ctx);
+	while(buffIdx < nbElements) {
+		crc32_update(&ctx, byteBuff[buffIdx], 1);
+		buffIdx++;
+	}
+	crc32_final(&ctx);
+	sprintf(fileprint, "%08x", ctx.state);
+    return fileprint;
+}
