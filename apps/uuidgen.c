6530f76805258eaf61fe6ba6344e0b486a0b8e12
jsmithnh09 <jsmithnh09@gmail.com>
2023-10-11 21:50:41 -0400

    Adding perms and namespace ID opt.

--- apps/uuidgen.c
+++ apps/uuidgen.c
@@ -20,16 +20,21 @@
 *   -n <number-of-ids>      = prints "n" number of IDs. Deafult is 1.
 ******************************************************************/
 
-
-/*********************************************************
+/**********************************************************************
 * UUIDGEN [NUM_IDS=1] [VERSION=4] ...VisualStudio, NO getopt???
 * Uses:
-*   UUIDGEN 3 <"INPUT-STR">
+*   UUIDGEN 3 <"INPUT-STR"> [NAMESPACE-ID <0-3>]
 *   UUIDGEN 4 <NUM-IDs>
-*   UUIDGEN 5 <"INPUT-STR">
+*   UUIDGEN 5 <"INPUT-STR"> [NAMESPACE-ID <0-3>]
+*
+* UUIDGEN versions 3 and 5 accept an optional Namespace ID, where:
+*   0 -- DNS namespace
+*   1 -- URL namespace
+*   2 -- Object-ID (OID) namespace
+*   3 -- X.500 namespace 
 *
 * Default, no args: UUIDGEN 4 1
-**********************************************************/
+**********************************************************************/
 int main(int argc, char *argv[]) {
     if ((argc == 2) && (!strncmp(argv[1], "-v", 2) || !strncmp(argv[1], "--v", 3))) {
         printf("file-hash: uuidgen v%s\n", FILE_HASH_VERSION);
@@ -40,9 +45,10 @@ int main(int argc, char *argv[]) {
         printf("Version 4 uses crypto-secure random bytes to build the identifier.\n");
         return 0;
     }
-    if (argc == 3) {
+    if (argc > 2) {
         unsigned long numIds = 1;
         unsigned int vnum = 4;
+        unsigned int nsid = 0;
         if (sscanf(argv[1], "%d", &vnum) == -1) {
             fprintf(stderr, "Cannot parse UUID version number.\n");
             exit(1);
@@ -50,7 +56,29 @@ int main(int argc, char *argv[]) {
         char* uuid;
         switch(vnum) {
             case 3:
-                uuid = uuid3(STR_UUID_OID, argv[2]);
+                if (argc == 4) {
+                    if (sscanf(argv[3], "%d", &nsid) == -1) {
+                        fprintf(stderr, "Cannot parse namespace ID number.\n");
+                        exit(1);
+                    }
+                }
+                switch(nsid) {
+                    case 0:
+                        uuid = uuid3(STR_UUID_DNS, argv[2]);
+                        break;
+                    case 1:
+                        uuid = uuid3(STR_UUID_URL, argv[2]);
+                        break;
+                    case 2:
+                        uuid = uuid3(STR_UUID_OID, argv[2]);
+                        break;
+                    case 3:
+                        uuid = uuid3(STR_UUID_X500, argv[2]);
+                        break;
+                    default:
+                        uuid = uuid3(STR_UUID_DNS, argv[2]);
+                        break;
+                } // switch
                 printf("%.36s\n", uuid);
                 free(uuid);
                 return 0;
@@ -67,14 +95,30 @@ int main(int argc, char *argv[]) {
                 }
                 return 0;
             case 5:
-                uuid = uuid5(STR_UUID_OID, argv[2]);
+                switch(nsid) {
+                    case 0:
+                        uuid = uuid5(STR_UUID_DNS, argv[2]);
+                        break;
+                    case 1:
+                        uuid = uuid5(STR_UUID_URL, argv[2]);
+                        break;
+                    case 2:
+                        uuid = uuid5(STR_UUID_OID, argv[2]);
+                        break;
+                    case 3:
+                        uuid = uuid5(STR_UUID_X500, argv[2]);
+                        break;
+                    default:
+                        uuid = uuid5(STR_UUID_DNS, argv[2]);
+                        break;
+                } // switch
                 printf("%.36s\n", uuid);
                 free(uuid);
                 return 0;
             default:
                 fprintf(stderr, "Invalid version number %d.\n", (int)vnum);
                 exit(1);
-        }
+        } // switch
     } else if (argc == 1) {
         char *uuid = uuid4();
         printf("%.36s\n", uuid);
